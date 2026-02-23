ca9aa65ccea59c9637d1da9b2789c5d0d18bf240
Niles Johlke <nmjohlke@gmail.com>
2026-02-23 11:06:14 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,10 +1,27 @@
+import javax.swing.*;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
-        Restaurant mcDonalds = new Restaurant();
-        Restaurant chipotle = new Restaurant(30, 4.5, 14, 5, "6:00am - 9:00pm");
+        // Instantiate an ArrayList to hold Restaurant Objects
+        ArrayList<Restaurant> restaurantList = new ArrayList<>();
 
-        System.out.println(chipotle.waitTime);
-        System.out.println(chipotle.hoursOpen);
+        // Take user input using JOptionPane and store variables
+        String restaurantName = JOptionPane.showInputDialog("What would you like to name your Restaurant? ");
+        int waitTime = Integer.parseInt(JOptionPane.showInputDialog("Enter Restaurant Wait Time in Minutes: "));
+        double rating = Double.parseDouble(JOptionPane.showInputDialog("Enter Restaurant Rating (0.0 - 5.0): "));
+        double avgPrice = Double.parseDouble(JOptionPane.showInputDialog("What is the Average Price of your Restaurant? (Ex. 12.50) "));
+        double distance = Double.parseDouble(JOptionPane.showInputDialog("Enter Restaurant Distance In Miles (Ex. 6.7): "));
+        String hoursOpen = JOptionPane.showInputDialog("Enter the Hours of Operation (Ex. 6:00am - 9:00pm): ");
+
+
+        // Instantiate Restaurant object with those variables
+        Restaurant userRestaurant = new Restaurant(restaurantName, waitTime, rating, avgPrice, distance, hoursOpen);
+        System.out.println(userRestaurant);
+
+        // Add User's Restaurant object to the ArrayList
+        restaurantList.add(userRestaurant);
+
     }
 }
--- Restaurant.java
+++ Restaurant.java
@@ -1,19 +1,87 @@
 public class Restaurant {
+
     // Properties
-     int waitTime;
-     double rating;
-     double avgPrice;
-     double dist;
-     String hoursOpen;
+    private String restaurantName;
+    private int waitTime;
+    private double rating;
+    private double avgPrice;
+    private double distance;
+    private String hoursOpen;
 
     // Constructor
+
     public Restaurant() {}
 
-        public Restaurant(int waitTime, double rating, double avgPrice, double dist, String hoursOpen){
+    public Restaurant(String restaurantName, int waitTime, double rating, double avgPrice, double distance, String hoursOpen) {
+        this.restaurantName = restaurantName;
         this.waitTime = waitTime;
         this.rating = rating;
         this.avgPrice = avgPrice;
-        this.dist = dist;
+        this.distance = distance;
+        this.hoursOpen = hoursOpen;
+
+
+    }
+
+    //Getters & Setters
+    // Accessors & Mutators
+
+    public String getRestaurantName() {
+        return restaurantName;
+    }
+
+    public void setRestaurantName(String restaurantName) {
+        this.restaurantName = restaurantName;
+    }
+
+    public int getWaitTime() {
+        return waitTime;
+    }
+
+    public void setWaitTime(int waitTime) {
+        this.waitTime = waitTime;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public double getAvgPrice() {
+        return avgPrice;
+    }
+
+    public void setAvgPrice(double avgPrice) {
+        this.avgPrice = avgPrice;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public String getHoursOpen() {
+        return hoursOpen;
+    }
+
+    public void setHoursOpen(String hoursOpen) {
         this.hoursOpen = hoursOpen;
     }
+
+    public String toString(){
+        return "Restaurant Name: " + restaurantName +
+                "\nWait time in minuets: " + waitTime +
+                "\nRating: " + rating +
+                "\nAverage Price: $" + avgPrice +
+                "\nDistance: " + distance + "mi." +
+                "\nHours of Operations: " + hoursOpen;
+
+    }
+
 }
